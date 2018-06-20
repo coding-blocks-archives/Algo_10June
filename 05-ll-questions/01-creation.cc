2c7cd1ac943f2d93d519757c25be7e4d53138415
Deepak Aggarwal <daggarwal77@gmail.com>
2018-06-20 15:00:33 +0100

    Linked List

--- 05-ll-questions/01-creation.cc
+++ 05-ll-questions/01-creation.cc
@@ -45,31 +45,149 @@ void printLL(const Node* head){
     cout << endl;
 }
 
-??? deleteNode(Node* head, int dataToDelete){
+Node* deleteNode(Node* head, int dataToDelete){
+    Node* cur = head;
+    Node* previous = NULL;
+    while(cur){
+        if (cur->data == dataToDelete){
+            // I have found a node to be deleted
+            if (previous == NULL){
+                // head is to be deleted
+                head = cur->next;
+                delete cur;
+            }
+            else {
+                previous->next = cur->next; 
+                delete cur;
+            }
+            break;
+        }
+        else {
+            previous = cur;
+            cur = cur->next;
+        }
+    }
+    return head;
 }
-??? mergeSort(Node* head){
-}
-??? quickSort(Node* head){
-}
-??? bubbleSort(Node* head){
 
+Node* midPt(Node* head){    // Get
+    Node* slow = head;
+    Node* fast = head;  // Set
+    // GO
+    while(slow && fast && fast->next && fast->next->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
 }
-??? addOne(Node* head){
 
-}
-??? deleteDuplicates(Node* head){
+Node* mergeSortedLists(Node* a, Node* b){
+    if (a == NULL) return b;
+    if (b == NULL) return a;
 
+    if (a->data < b->data){
+        a->next = mergeSortedLists(a->next, b);
+        return a;
+    }
+    else {
+        b->next = mergeSortedLists(a, b->next);
+        return b;
+    }
 }
-??? findLoop(Node* head){
 
+Node* mergeSort(Node* head){
+    if (head == NULL || head->next == NULL){
+        return head;
+    }
+    Node* middle = midPt(head);
+    Node* l1 = head;
+    Node* l2 = middle->next;
+    middle->next = NULL;
+    l1 = mergeSort(l1);
+    l2 = mergeSort(l2);
+    Node* ans = mergeSortedLists(l1, l2);
+    return ans;
 }
-??? removeLoop(Node* head){
+// ??? quickSort(Node* head){
+// }
 
+int lenLL(Node* head){
+    Node* cur = head; 
+    int cnt = 0;
+    while(cur){ ++cnt; cur = cur->next;}
+    return cnt;
 }
+
+Node* bubbleSort(Node* head){
+    int len = lenLL(head);
+    int cnt = len;
+    for(int i = 0; i < len; ++i){
+        Node* cur = head;
+        Node* previous = NULL;
+        while(cnt && cur && cur->next){
+            Node* ahead = cur->next;
+            if (cur->data > ahead->data){
+                // swapping needs to be done
+                if (previous == NULL){
+                    cur->next = ahead->next;
+                    ahead->next = cur;
+                    head = ahead;
+                    previous = ahead;
+                }
+                else{
+                    cur->next = ahead->next; 
+                    previous->next = ahead;
+                    ahead->next = cur;
+                    previous = ahead;
+                }
+            }
+            else {
+                previous = cur;
+                cur = cur->next;
+            }
+        }
+        --cnt;
+    }
+    return head;
+}
+
+Node* reverseList(Node* head){
+    if (head == NULL) return;
+    Node* remListHead = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return remListHead;
+}
+
+Node* addOne(Node* head){
+
+// }
+// ??? deleteDuplicates(Node* head){
+
+// }
+// ??? findLoop(Node* head){
+
+// }
+// ??? removeLoop(Node* head){
+
+// }
 
 
 int main(){
+    // Node* head = createLL();
+    // printLL(head);
+    // int x; cin >> x;
+    // head = deleteNode(head, x);
+    // printLL(head);
+
+    // Node* head = createLL();
+    // printLL(head);
+    // head = mergeSort(head);
+    // printLL(head);
+
     Node* head = createLL();
+    printLL(head);
+    head = bubbleSort(head);
     printLL(head);
        
 }
