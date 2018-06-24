d6d4726d007c2d58f49cb59267b50a37b2fca092
Deepak Aggarwal <daggarwal77@gmail.com>
2018-06-24 12:57:54 +0100

    /

--- 07-trees/02-tree.cc
+++ 07-trees/02-tree.cc
@@ -9,10 +9,12 @@ public:
     int data;
     TreeNode* left;
     TreeNode* right;
+    TreeNode* next;
     TreeNode(int d){
         data = d;
         left = NULL;
         right = NULL;
+        next = NULL;
     }
 };
 
@@ -46,17 +48,69 @@ int height(const TreeNode* root){
 
 void levelPrint(TreeNode* root){
     if (root == NULL) return;
+    TreeNode* const MARKER = NULL;
 
     queue<TreeNode*> q;
     q.push(root);
+    q.push(MARKER);
 
     while(q.empty() == false){
         TreeNode* cur = q.front(); q.pop();
+        if (cur == MARKER){
+            // cur level is processed....all children of this level are processed
+            // this signfies next level has ended
+            cout << endl;
+            if (q.empty() == false) q.push(MARKER);
+            continue;
+        }
+
         cout << cur->data << " ";
         if (cur->left) q.push(cur->left);
         if (cur->right) q.push(cur->right);
     }
 }
+
+int diameter1(TreeNode* root){
+    if (root == NULL) return 0;
+
+    int leftDia = diameter1(root->left);
+    int rightDia = diameter1(root->right);
+    int curDia = height(root->left) + height(root->right) + 1;
+    return max(max(leftDia, rightDia), curDia);
+}
+
+class Pair{
+public:
+    int diameter;
+    int height;
+    Pair(int d, int h){ diameter = d; height = h;}
+};
+
+Pair diameter2(TreeNode* root){
+    if (root == NULL){
+        return Pair(0, 0);
+    }
+
+    Pair lt = diameter2(root->left);
+    Pair rt = diameter2(root->right);
+    int curHt = max(lt.height, rt.height) + 1;
+    int curDia = leftHt + rightHt + 1;
+    return Pair(curDia, curHt);
+}
+
+int heightWithDiameter(TreeNode* root, int& diameterComputedSoFar){
+    if (root == NULL){
+        return 0;
+    }
+
+    int lt = heightWithDiameter(root->left, diameterComputedSoFar);
+    int rt = heightWithDiameter(root->right, diameterComputedSoFar);
+    int curDia = lt + rt + 1;
+    diameterComputedSoFar = max(diameterComputedSoFar, curDia);
+    return max(lt, rt) + 1;
+}
+
+
 
 int main(){
     // TreeNode* root = createTree();
