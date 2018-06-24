91aceb8d85ff784370d02f4d3f6c375fbc30825d
Deepak Aggarwal <daggarwal77@gmail.com>
2018-06-24 09:52:23 +0100

    Level wise printing

--- 07-trees/02-tree.cc
+++ 07-trees/02-tree.cc
@@ -1,14 +1,18 @@
 // Deepak Aggarwal, Coding Blocks
 // deepak@codingblocks.com
 #include <iostream>
+#include <queue>
 using namespace std;
 
 class TreeNode{
+public:
     int data;
     TreeNode* left;
     TreeNode* right;
     TreeNode(int d){
         data = d;
+        left = NULL;
+        right = NULL;
     }
 };
 
@@ -19,13 +23,48 @@ TreeNode* createTree(){
     }
 
     TreeNode* root = new TreeNode(x);
+    // cout << "Enter left child of " << x << " ";
     root->left = createTree();
+    // cout << "Enter right child of " << x << " ";
     root->right = createTree();
     return root;
 }
 
+void inorder(const TreeNode* root){
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int height(const TreeNode* root){
+    if (root == NULL) return 0;
+    int leftHt = height(root->left);
+    int rightHt = height(root->right);
+    return max(leftHt, rightHt) + 1;
+}
+
+void levelPrint(TreeNode* root){
+    if (root == NULL) return;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(q.empty() == false){
+        TreeNode* cur = q.front(); q.pop();
+        cout << cur->data << " ";
+        if (cur->left) q.push(cur->left);
+        if (cur->right) q.push(cur->right);
+    }
+}
 
 int main(){
+    // TreeNode* root = createTree();
+    // inorder(root);   
+    // cout << "\n===========\n";
+    // int ans = height(root);
+    // cout << ans; 
+
     TreeNode* root = createTree();
-    printTree(root);    
+    levelPrint(root);
 }
